3db9680cb4674a10670c60303d0734d17b189c66
Alex Forrence <alex.forrence@gmail.com>
2021-09-28 13:15:12 -0400

    bump server secret

--- netlify_functions/faunadb.js
+++ netlify_functions/faunadb.js
@@ -5,7 +5,7 @@ const q = faunadb.query
 exports.handler = async(event, context) => {
   // create client w/ secret
   const client = new faunadb.Client({
-    secret: process.env.FAUNADB_SERVER_SECRET_V2,
+    secret: process.env.FAUNADB_SERVER_SECRET_V3,
     domain: 'db.us.fauna.com',
     scheme: 'https'
   })
